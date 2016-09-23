c3962c5ef905b7b02e0b304a79202264a82f33a8
crabcrabcam <cam12win@gmail.com>
2016-09-23 12:04:11 +0100

    Initial Commit
    Basics are all there

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public string levelToLoad;
+
+	public GameObject currentCheckpoint;
+
+	private PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RespawnPlayer()
+	{
+		Debug.Log("Player Respawn");
+		player.transform.position = currentCheckpoint.transform.position;
+	}
+
+
+	public void NextLevel()
+	{
+		Debug.Log("Level Up!");
+		Application.LoadLevel(levelToLoad);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	public float speed;
+	public float jumpHeight;
+
+	public LayerMask whatIsGround;
+	public float groundCheckRadius;
+	public Transform groundCheck;
+	private bool grounded;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+
+		Movement ();
+		GroundCheck ();
+
+	}
+
+
+	void Movement() {
+
+		if (Input.GetAxisRaw ("Horizontal") > 0.1) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		} else if (Input.GetAxisRaw ("Horizontal") < -0.1) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		} else if (Input.GetAxisRaw("Horizontal") == 0 && grounded) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
+
+		}
+
+
+		if (Input.GetAxisRaw ("Vertical") > 0.5 && grounded) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+
+		}
+
+	}
+
+	void GroundCheck()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+}
